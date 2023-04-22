3886a22672f09b4c1ad4acdcba9415c64add72d8
mh.lee <mh.lee239@gmail.com>
2023-04-22 13:00:39 +0900

    Init & Route Setting

---
+++ Shopping-mall/src/App.tsx
@@ -0,0 +1,11 @@
+import {useRoutes} from 'react-router-dom'
+import {routes} from './routes'
+
+const App = () => {
+    const elem = useRoutes(routes)
+    return (
+        elem
+    )
+}
+
+export default App;
---
+++ Shopping-mall/src/routes.tsx
@@ -0,0 +1,27 @@
+
+import GlobalLayout from './pages/_layout'
+import MainPage from './pages/index'
+import ProductList from './pages/products/index'
+import ProductDetail from './pages/products/[id]'
+
+
+export const routes = [
+    {
+        path:"/",
+        element: <GlobalLayout />,
+        children: [
+            { path : "/", element: <MainPage />, index:true },
+            { path : "/products", element: <ProductList />, index:true },
+            { path : "/products/:id", element: <ProductDetail /> },
+            // { path : "/", element: <DynamicIndex />, index:true },
+            // { path : "/", element: <DynamicIndex />, index:true },
+        ]
+    }
+]
+
+export const pages = [
+    {route : '/'},
+    {route : '/products'},
+    {route : '/products/:id'},
+    
+]
